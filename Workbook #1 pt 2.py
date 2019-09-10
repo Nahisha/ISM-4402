c00f61356d07256f031d887e9d16abcdff2e10e2
Nahisha <54560288+Nahisha@users.noreply.github.com>
2019-09-09 20:52:26 -0400

    Add files via upload

---
+++ Workbook #1 pt 2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+
+Location = "Documents/School work/ISM 4402 Business Intelligence/datasets/gradedata.xlsx"
+df = pd.read_excel(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
