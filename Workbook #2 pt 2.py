229028756f025b9936f5ae7dd6c6851228447842
Nahisha <54560288+Nahisha@users.noreply.github.com>
2019-10-21 12:38:17 -0400

    Add files via upload

--- Workbook #2 pt 2.py
+++ Workbook #2 pt 2.py
@@ -8,7 +8,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # Connect to sqlite db
-db_file = r'Documents/ School work/ISM 4402 Business Intelligence/datasets/gradedata.db'
+db_file = r'Documents/School work/ISM 4402 Business Intelligence/datasets/gradedata.db'
 engine = create_engine(r"sqlite:///{}".format(db_file))
 
 sql = 'SELECT * from test where Grades in (76,77,78)'
